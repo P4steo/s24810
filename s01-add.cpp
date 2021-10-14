ed6b13962fd6453a9c732e3e28e09b8add12c07c
Pablo <91740791+P4steo@users.noreply.github.com>
2021-10-14 16:33:01 +0200

    Add files via upload

---
+++ s01-add.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+auto main(int argc, char* argv[]) -> int
+{
+	auto const a=std::stoi(argv[1]);
+	auto const b=std::stoi(argv[2]);
+	std::cout<<(a+b)<<"\n";
+	return 0;
+}
+
---
+++ s01-sud.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+auto main(int argc, char* argv[]) -> int
+{
+	auto const a = std::stoi(argv[1]);
+	auto const b = std::stoi(argv[2]);
+	std::cout<<(a-b)<<std::endl;
+	return 0;
+}
