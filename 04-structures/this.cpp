b47c423244276d004eb19af89e55ede11f3af9bf
P4steo <s24810@pjwstk.edu.pl>
2021-12-07 16:50:19 +0100

    This

---
+++ 04-structures/this.cpp
@@ -0,0 +1,9 @@
+#include "this.h"
+
+int main() {
+	This sth;
+	sth.This::funkcja();
+	std::cout<<&sth;
+
+return 0;
+}
---
+++ 04-structures/this.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+struct This{
+	auto funkcja(){
+	std::cout<<this;
+	}
+};
+
